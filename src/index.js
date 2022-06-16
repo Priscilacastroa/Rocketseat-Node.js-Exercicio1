a41cebcc08a94f2c2f6323f41618f872d314e1dc
Priscila Castro <priscilacastroa@gmail.com>
2022-06-15 22:25:47 -0300

    params

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ app.get("/courses", (request, response) => {
     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
-
 app.post("/courses", (request, response) => {
     const body = request.body;
     console.log(body);
